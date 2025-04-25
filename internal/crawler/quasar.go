84788479b45365b931899b5ff244519a3c9df91c
Junseok Shin <sjsage522@gmail.com>
2025-04-25 12:00:32 +0900

    Feat quasar price added

--- internal/crawler/quasar.go
+++ internal/crawler/quasar.go
@@ -4,10 +4,21 @@ import (
 	"sjsage522/hotdealworker/config"
 	"sjsage522/hotdealworker/helpers"
 	"sjsage522/hotdealworker/services/cache"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 // NewQuasarCrawler creates a Quasar crawler
 func NewQuasarCrawler(cfg config.Config, cacheSvc cache.CacheService) *UnifiedCrawler {
+	priceHandler := func(s *goquery.Selection) string {
+		priceSel := s.Find("div.market-info-sub p span span")
+		if priceSel.Length() == 0 {
+			return ""
+		}
+		return strings.TrimSpace(priceSel.Text())
+	}
+
 	return NewUnifiedCrawler(CrawlerConfig{
 		URL:          cfg.QuasarURL + "/bbs/qb_saleinfo",
 		CacheKey:     "quasar_rate_limited",
@@ -17,12 +28,12 @@ func NewQuasarCrawler(cfg config.Config, cacheSvc cache.CacheService) *UnifiedCr
 		UseChrome:    false,
 		ChromeDBAddr: cfg.ChromeDBAddr,
 		Selectors: Selectors{
-			DealList:   "div.market-type-list.market-info-type-list.relative table tbody tr",
-			Title:      "div.market-info-list-cont p.tit a.subject-link span.ellipsis-with-reply-cnt",
-			Link:       "div.market-info-list-cont p.tit a.subject-link",
-			Thumbnail:  "div.market-info-list div.thumb-wrap a.thumb img.maxImg",
-			PostedAt:   "span.date",
-			PriceRegex: `([0-9,]+원)`,
+			DealList:      "div.market-type-list.market-info-type-list.relative table tbody tr",
+			Title:         "div.market-info-list-cont p.tit a.subject-link span.ellipsis-with-reply-cnt",
+			Link:          "div.market-info-list-cont p.tit a.subject-link",
+			Thumbnail:     "div.market-info-list div.thumb-wrap a.thumb img.maxImg",
+			PostedAt:      "span.date",
+			PriceHandlers: []ElementHandler{priceHandler},
 		},
 		IDExtractor: func(link string) (string, error) {
 			return helpers.GetSplitPart(link, "/", 6)
